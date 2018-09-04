6eb0c6f8ee67cce373eafc572f1432859d63b1e1
Keith Dahlby <keith@newbo.co>
2018-09-04 12:41:13 -0500

    Add auth middleware with mock User model

--- __tests__/auth/middleware.test.js
+++ __tests__/auth/middleware.test.js
@@ -1,6 +1,56 @@
 'use strict';
 
+jest.mock('../../src/auth/model');
+
 import auth from '../../src/auth/middleware';
 
 describe('Auth Middleware', () => {
+  it('works for correct username and password', done => {
+    let user = 'dahlbyk';
+    let password = 'DAHLBYK';
+
+    // btoa = Base64 encode username:password
+    let code = btoa(`${user}:${password}`);
+
+    let req = {
+      headers: {
+        authorization: `Basic ${code}`,
+      },
+    };
+    let res = {};
+
+    // auth = middleware, which receives request, response, next
+    auth(req, res, () => {
+      expect(res.token).toBe('dahlbyk token!');
+      done();
+    });
+  });
+
+  it('returns error when the auth header is not present', done => {
+    let req = { headers: {} };
+    let res = {};
+
+    auth(req, res, (err) => {
+      expect(err).toBeDefined();
+      expect(err.status).toBe(401);
+      done();
+    });
+  });
+
+  it('returns error when the auth header is invalid Basic auth', done => {
+    let req = {
+      headers: {
+        authorization: 'Basic Basic',
+      },
+    };
+    let res = {};
+
+    auth(req, res, (err) => {
+      expect(err).toBeDefined();
+      expect(err.status).toBe(401);
+      done();
+    });
+  });
+
+  // TODO: real username/password mismatch
 });
