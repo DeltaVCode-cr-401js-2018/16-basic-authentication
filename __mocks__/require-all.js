baa45b0ee5d3c07df79d79c085dd80c60b986bf2
JohnMV13 <thejohnmiesterV@gmail.com>
2018-09-04 16:12:02 -0500

    finished adding in the class demo

---
+++ __mocks__/require-all.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = (dir) => {
+  if (typeof dir !== 'string') {
+    throw new Error('require-all needs dir');
+  }
+
+  return {
+    'foo': { default: fakeModel('foo', 'foo-route') },
+    'bar': { default: fakeModel('bar') },
+  };
+};
+
+function fakeModel(modelName, route) {
+  class Fake {
+    constructor() {
+    }
+
+    static findById(id) {
+      return new Fake();
+    }
+    
+    // TODO: other things we need from Mongoose
+  }
+
+  Fake.modelName = modelName;
+  Fake.route = route;
+  return Fake;
+}
