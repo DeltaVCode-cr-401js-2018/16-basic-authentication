a1a005784f25861749fba7a94f3867557e0a1e3d
Keith Dahlby <keith@newbo.co>
2018-09-06 16:52:38 -0500

    Complete OAuth flow to get email from GitHub

--- src/auth/router.js
+++ src/auth/router.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const debug = require('debug')('app/auth/router');
+import superagent from 'superagent';
 
 import express from 'express';
 const authRouter = express.Router();
@@ -42,6 +43,42 @@ authRouter.get('/login/github', (req, res) => {
 
   debug(`redirecting to GitHub with scope ${scope}`);
   res.redirect(`${gitHubUrl}?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}`);
+});
+
+authRouter.get('/oauth/github/code', (req, res, next) => {
+  const { code } = req.query;
+  debug('OAuth code response', req.query);
+  if (!code) {
+    res.redirect(process.env.CLIENT_URL);
+    return;
+  }
+
+  const body = {
+    grant_type: 'authorization_code',
+    code, // code: code,
+    redirect_uri: `${CLIENT_URL}/oauth/github/code`,
+    client_id: GITHUB_CLIENT_ID,
+    client_secret: process.env.GITHUB_CLIENT_SECRET,
+  };
+  debug(body);
+
+  superagent
+    .post('https://github.com/login/oauth/access_token')
+    .set('Accept', 'application/json')
+    .type('form')
+    .send(body)
+    // GitHub Token Response
+    .then(response => {
+      const gitHubToken = response.body.access_token;
+
+      return superagent.get('https://api.github.com/user/emails')
+        .set('Authorization', `Bearer ${gitHubToken}`)
+        .then(emailResponse => {
+          var primaryEmail = emailResponse.body.find(email => email.primary);
+          res.redirect('http://localhost:5500?email=' + primaryEmail.email);
+        });
+    })
+    .catch(next);
 });
 
 export default authRouter;
