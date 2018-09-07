daa5ab3dd2641442e6fd065db32f292daaf02057
Keith Dahlby <keith@newbo.co>
2018-09-07 12:31:52 -0500

    Implement S3 upload

--- src/routes/pic.js
+++ src/routes/pic.js
@@ -9,19 +9,35 @@ export default router;
 
 import Pic from '../models/pic';
 import Gallery from '../models/gallery';
-import uuid from 'uuid';
 
 const dataDir = `${__dirname}/../../temp`;
 import multer from 'multer';
 const upload = multer({ dest: dataDir });
 
 import del from 'del';
+import fs from 'fs';
+//import path from 'path';
+import AWS from 'aws-sdk';
+
+AWS.config.setPromisesDependency(Promise);
+
+// Promisify S3 Upload
+const s3uploadAsync = (options) => {
+  const s3 = new AWS.S3();
+  return new Promise((resolve, reject) => {
+    s3.upload(options, (err, data) => {
+      if (err) return reject(err);
+      else return resolve(data);
+    });
+  });
+};
 
 router.post('/gallery/:id/pic', upload.single('image'), (req, res, next) => {
   debug(`POST /gallery/${req.params.id}/pic`);
 
   // File not uploaded!
   if (!req.file) {
+    debug('File missing!');
     return next({ status: 400 });
   }
 
@@ -36,17 +52,32 @@ router.post('/gallery/:id/pic', upload.single('image'), (req, res, next) => {
     del([req.file.path]);
   });
 
+  // Turns out we don't need this...not sure why it's in the demo...
+  // but this is how you can get a file extension!
+  // req.file.ext = path.extname(req.file.originalname);
+  // debug(req.file);
+
   Gallery.findById(req.params.id)
     .then(gallery => {
       if (!gallery)
         return next();
 
+      let s3options = {
+        ACL: 'public-read',
+        Bucket: process.env.AWS_BUCKET,
+        Key: `${req.file.filename}-${req.file.originalname}`, // deadbeef-logo.png
+        ContentType: req.file.mimetype,
+        Body: fs.createReadStream(req.file.path),
+      };
+      debug({ s3options });
+
+      return s3uploadAsync(s3options);
     })
-    .then(() => {
+    .then(s3data => {
       return new Pic({
         ...req.body,
-        objectKey: uuid(),
-        imageURI: `https://example.com/${uuid()}`,
+        objectKey: s3data.Key,
+        imageURI: s3data.Location,
         galleryID: req.params.id,
         userID: req.user._id,
       }).save();
